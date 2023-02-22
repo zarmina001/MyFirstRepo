a15690019eb627e7960280ac9ce0b19f86ea77cf
Zarmina <walizaizarmina@gmail.com>
2023-02-22 17:18:32 -0500

    new method added and old one deleted

--- src/GitTest/GitPracticeTwo.java
+++ src/GitTest/GitPracticeTwo.java
@@ -3,8 +3,11 @@ package GitTest;
 public class GitPracticeTwo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("This is for Push practice");
+	
 
+	
+	
 	}
-
 }
