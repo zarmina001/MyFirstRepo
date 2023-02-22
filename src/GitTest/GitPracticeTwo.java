95c8311f6c4011e977fb1e388fa7b0a442a24a6e
Zarmina <walizaizarmina@gmail.com>
2023-02-22 16:25:09 -0500

    This is for push practice

---
+++ src/GitTest/GitPracticeTwo.java
@@ -0,0 +1,10 @@
+package GitTest;
+
+public class GitPracticeTwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/GitTest/UnitTest.java
@@ -0,0 +1,20 @@
+package GitTest;
+
+public class UnitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//int a = 20;
+		//int b = 40;
+		//int c = a*b;
+		
+		//System.out.print(c);
+		System.out.println(testOne(40,10));
+
+	}
+	public static int testOne(int a, int b) {
+		return a-b;
+	}
+
+}
