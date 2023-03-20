0eb08af148f94029383d69b45b0930a04ba79d9e
Zarmina <walizaizarmina@gmail.com>
2023-03-20 00:01:42 -0400

    this is my first commit

--- src/GitTest/GitPracticeTwo.java
+++ src/GitTest/GitPracticeTwo.java
@@ -5,9 +5,6 @@ public class GitPracticeTwo {
 	public static void main(String[] args) {
 		
 		System.out.println("This is for Push practice");
-	
-
-	
-	
+		System.out.println("This is for pull practice");
 	}
 }
---
+++ src/GitTest/YotubVedios.java
@@ -0,0 +1,10 @@
+package GitTest;
+
+public class YotubVedios {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
